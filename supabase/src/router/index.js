55cf565edf1de8638c6e81dd64f69a68a262e9ad
Boxer300 <romano2789@nycstudents.net>
2026-04-27 12:30:35 -0400

    zghv

--- supabase/src/router/index.js
+++ supabase/src/router/index.js
@@ -1,8 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import login.vue from '@/views/login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: '
+      component: 
+    },
+    {
+      path: 
+      name: 
+      component: 
+    },
+    {
+      path: '/vue-lists',
+      name: 'vue-lists',
+      component: login,
+    },
+  ],
 })
 
 export default router
---
+++ supabase/src/views/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <form action="submit" @submit.prevent="login(user)">
+      <label for="username">Username</label>
+      <input type="text" v-model="user.username" />
+      <label for="password">Password</label>
+      <input type="password" v-model="user.password" />
+      <button type="submit">Login</button>
+    </form>
+    <h2 v-if="loggedIn">Welcome {{ user.username }}</h2>
+    <h2 v-else>Please Login</h2>
+  </div>
+</template>
+
+<script setup>
+//add new ref for loggedIn: Boolean. new function to update that value
+import { reactive, ref } from 'vue'
+const user = reactive({ username: '', password: '' })
+const loggedIn = ref(false)
+function login(user) {
+  console.log(user)
+  loggedIn.value = true
+}
+</script>
+
+<style lang="scss" scoped></style>
