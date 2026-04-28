bf73793e113898af82e112300b167d6704c53ba3
veeveejeebies <vavinz@nycstudents.net>
2026-04-28 12:22:54 -0400

    k

--- supabase/src/App.vue
+++ supabase/src/App.vue
@@ -6,6 +6,28 @@
 
 <script setup>
 
+import { ref, onMounted } from 'vue'
+import { supabase } from './supabase'
+
+const transactions = ref([])
+const error = ref(null)
+
+onMounted(async () => {
+  let { data: transactionData, error: err } = await supabase
+    .from('transactions')
+    .select('*')
+  if (err) {
+    error.value = err.message
+  } else {
+    transactions.value = transactionData
+  }
+})
+
+
+
+
+
+
 </script>
 
 <style lang="scss" scoped>
