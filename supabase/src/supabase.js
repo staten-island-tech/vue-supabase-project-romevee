b725cfa9f98f15012cfc8e46a580ce08def6e518
veeveejeebies <vavinz@nycstudents.net>
2026-06-01 12:29:44 -0400

    Update supabase.js

--- supabase/src/supabase.js
+++ supabase/src/supabase.js
@@ -1,9 +1,8 @@
-
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://YOUR_PROJECT_ID.supabase.co'
+const supabaseUrl = 'https://afrofrndlppknobgekhh.supabase.co/rest/v1/posting1'
 
-const supabaseKey = 'YOUR_ANON_PUBLIC_KEY'
+const supabaseKey = 'sb_publishable_IlhV2YSg_N2tCXJ-Vspa7g_wpDoP5l0'
 
 export const supabase = createClient(
   supabaseUrl,
