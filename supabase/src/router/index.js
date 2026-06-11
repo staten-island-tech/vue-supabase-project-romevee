bdd570e75ead1cd12e212d0c14d63e436e1b64c6
veeveejeebies <vavinz@nycstudents.net>
2026-06-11 11:51:54 -0400

    pl

--- supabase/src/router/index.js
+++ supabase/src/router/index.js
@@ -1,27 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import home from "../views/home.vue"
-import login from "../views/login.vue"
-import signup from "../views/signup.vue"
+import Home from '../views/home.vue'
+import Login from '../views/login.vue'
+import Signup from '../views/signup.vue'
+
+// 1. Correct the path to point to the src folder, not views
+import UploadDashboard from '../App.vue' 
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/upload',
+    name: 'Upload',
+    // 2. Use the exact variable name you imported above
+    component: UploadDashboard, 
+    meta: { requiresAuth: true }
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: '',
-      component: home
-    },
-    {
-  path: '/signup',
-  name: 'signup',
-  component: signup
-   },
-    {
-      path: '/login',
-      name: 'login',
-      component: login
-    }
-  ],
+  history: createWebHistory(),
+  routes
 })
 
 export default router
