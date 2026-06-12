fe69f1db5c9d18c5ee6cce8d4231367d778e6397
Boxer300 <romano2789@nycstudents.net>
2026-06-12 12:07:14 -0400

    Update App.vue
    tr

--- supabase/src/App.vue
+++ supabase/src/App.vue
@@ -1,65 +1,13 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './supabase'
-
-const users = ref([])
-const error = ref(null)
-const loading = ref(true)
-
-onMounted(async () => {
-  let { data, error: err } = await supabase
-    .from('users')
-    .select('*')
-    .order('id', { ascending: false })
-
-  if (err) {
-    error.value = err.message
-  } else {
-    users.value = data
-  }
-
-  loading.value = false
-})
-</script>
 <template>
-  <div class="app">
-
-    <h1>Instagram Clone</h1>
-
-    <p v-if="loading">
-      Loading posts...
-    </p>
-
-    <p v-if="error">
-      {{ error }}
-    </p>
-
-    <div
-      v-for="user in users"
-      :key="user.id"
-      class="post-card"
-    >
-      <img
-        :src="user.image_url"
-        class="post-image"
-      />
-
-      <div class="post-content">
-        <h3>@{{ user.username }}</h3>
-        
-        <p>
-          {{ user.caption }}
-        </p>
-
-        <button>
-          ❤️ Like
-        </button>
-      </div>
-    </div>
-
+  <div>
+<router-view></router-view>
   </div>
-  <router-view></router-view>
 </template>
-<style lang="scss" scoped>
+
+<script setup>
+
+</script>
+
+<style scoped>
 
 </style>
